d047ac2e35d399c4e1b4b7ecad616acb69ddd910
RomaRoma <v12nes11@gmail.com>
2021-03-30 18:59:26 +0300

    html3.9

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-document.getElementById('nav').onmouseover = function(event){
-    var target = event.target;
-    if(target.className == 'menu_item') {
-        var s = target.getElementsByClassName('submenu');
-        s[0].style.display='block';
-    }
-}
+// document.getElementById('nav').onmouseover = function(event){
+//     var target = event.target;
+//     if(target.className == 'menu_item') {
+//         var s = target.getElementsByClassName('submenu');
+//         s[0].style.display='block';
+//     }
+// }
 
-document.onmouseover=function (event) {
-    var target = event.target;
-    console.log(event.target);
-    if (target.className != 'menu-item' && target.className !='submenu'{
-        closeMenu();
-    })
-}
+// document.onmouseover=function (event) {
+//     var target = event.target;
+//     console.log(event.target);
+//     if (target.className != 'menu-item' && target.className !='submenu'{
+//         closeMenu();
+//     })
+// }
 
-function closeMenu() {
-    var menu = document.getElementById('nav');
-    var subm = document.getElementsByClassName('submenu');
-    for (var i=0; i < subm.length; i++) {
-        subm[i].style.display='none';
-    }
-}
+// function closeMenu() {
+//     var menu = document.getElementById('nav');
+//     var subm = document.getElementsByClassName('submenu');
+//     for (var i=0; i < subm.length; i++) {
+//         subm[i].style.display='none';
+//     }
+// }
 
--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,29 @@
+/*POPUP*/
+$(document).ready(function() { // Ждём загрузки страницы
+	
+	$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+		$(".popup").fadeOut(800);
+		$(".form_body")[0].reset();	// Медленно убираем всплывающее окно
+	});
+});
+
+function showPopup() {
+	$(".popup").fadeIn(800); // Медленно выводим изображение
+}
+
+function hidePopup() {
+	$(".popup").fadeOut(800);
+	$(".form_body")[0].reset();
+}
+
 $(document).ready(function(){
     $('.slider').slick({
         prevArrow: $('.prev'),
         nextArrow: $('.next'),
         arrows: true,
         infinite: true,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 4000,
         fade: true
     });
 }); 
-
-/*POPUP*/
-$(document).ready(function() { // Ждём загрузки страницы
-	
-	$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
-		$(".popup").fadeOut(800);	// Медленно убираем всплывающее окно
-	});
-
-});
-
-function showPopup() {
-	$(".popup").fadeIn(800); // Медленно выводим изображение
-}
